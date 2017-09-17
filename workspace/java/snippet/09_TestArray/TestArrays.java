f655d469fcb68a277a408c47bb7b61a207c66076
rayluoluo <canweiluo@163.com>
2017-09-17 23:11:00 +0800

    add server project

---
+++ workspace/java/snippet/09_TestArray/TestArrays.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class TestArrays {
+	public static void main(String[] args) {
+		// 创建样本数组
+		int a[] = { 6, 23, 45, 12, -83, 94, 41 };
+		// 数组排序 （内部实现采用快速排序法）
+		Arrays.sort(a);
+		// 排序后数组元素输出
+		for (int i = 0; i < a.length; i++) {
+			System.out.print(a[i] + "\t");
+		}
+	}
+}
